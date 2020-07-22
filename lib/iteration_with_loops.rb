d77ff38a0de8d9387ece56f7428c0b03ecf24e10
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-22 14:25:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,9 @@ row_index = 0
 
 while row_index < src.count do
   element_index = 0
+  min_value = 1000
   while element_index < src[row_index].count do
-    temp = 1000
+
     if src[row_index][element_index] < temp
        src[row_index][element_index] = temp
     end
